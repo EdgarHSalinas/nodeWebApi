76d886161fa9cab6e64a6b6e1759f75dc0e54e3a
Edgar Salinas <edgar@my.codergv.org>
2016-10-21 12:55:57 -0500

    update addings cats

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-var http = require('http');
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {
+    extended: true
+}))
+
+var cats = require('./cats.js')(app);
+
+var server = app.listen(3000, function () {
+    console.log('Server Running at local 3000');
+});
+
+
+
+/*var http = require('http');
 
 http.createServer(function(req, res) {
       res.writeHead(200, {
@@ -8,3 +27,4 @@ http.createServer(function(req, res) {
 }).listen(3000, '127.0.0.01');
 
 console.log('Server Running at ');
+*/
